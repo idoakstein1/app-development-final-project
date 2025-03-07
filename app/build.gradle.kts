01e198533fcb4db67644af6d2c2c71da1d053dba
idoakstein1 <idoakstein1@gmail.com>
2025-03-07 14:45:22 +0200

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
 
     implementation(libs.androidx.swiperefreshlayout)
 
